5c1430c390964d4080c05afa63bff20881359924
Hyuan Peixoto <hyuanpeixoto@gmail.com>
2020-08-10 21:01:56 -0300

    first commit

---
+++ Assets/AnimActions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimActions : MonoBehaviour
+{
+
+    [SerializeField]
+    private Animator cubeAnim;
+
+    [SerializeField]
+    private string enterState;
+    [SerializeField]
+    private string exitState;
+    public void OnGazeEnter(){
+        cubeAnim.Play(enterState);
+        CameraPointer.instance.SetEnterMaterial();
+    }
+
+    public void OnGazeExit(){
+        cubeAnim.Play(exitState);
+        CameraPointer.instance.SetExitMaterial();
+    }
+
+
+}

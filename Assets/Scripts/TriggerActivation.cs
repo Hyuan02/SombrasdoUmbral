67d0ae103585197e1a0dae08f2994929b404cbac
Hyuan Peixoto <hyuanpeixoto@gmail.com>
2020-09-05 15:07:39 -0300

    anim sound developed

---
+++ Assets/Scripts/TriggerActivation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerActivation : MonoBehaviour
+{
+    bool changed = false;
+
+    [SerializeField]
+    private GameObject objectToActivate;
+    // Update is called once per frame
+    void Update()
+    {
+        if(!changed){
+            if(this.transform.parent.name == "CurrentPanel"){
+                objectToActivate.SetActive(true);
+                changed = true;            
+            }
+        }   
+    }
+}
---
+++ Assets/Scripts/TriggerSound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSound : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource sourceToChange;
+
+    [SerializeField]
+    private AudioClip clipToTrigger;
+
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float volumeToSwitch = 0.5f;
+
+    bool changed = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!changed){
+            if(this.transform.parent.name == "CurrentPanel"){
+                sourceToChange.Stop();
+                sourceToChange.clip = clipToTrigger;
+                sourceToChange.volume = volumeToSwitch; 
+                sourceToChange.Play();
+                changed = true;
+            }
+        }
+    }
+}

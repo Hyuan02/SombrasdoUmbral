050e2365cd6cfe122d30ea72627cdabcfc4cd144
Hyuan Peixoto <hyuanpeixoto@gmail.com>
2020-09-24 21:03:23 -0300

    sound trigger

--- Assets/Scripts/Triggers/TriggerActivation.cs
+++ Assets/Scripts/Triggers/TriggerActivation.cs
@@ -12,14 +12,23 @@ public class TriggerActivation : MonoBehaviour
     [SerializeField]
     float atTime = 0.0f;
 
+    [SerializeField]
+    [Range (0.0f, 20.0f)]
+    float onTime = 1.0f;
+
     // Update is called once per frame
     void Update()
     {
         if(!changed){
             if(this.transform.parent.name == "CurrentPanel"){
-                SoundManager.instance.Activate3DSound(index, atTime);
+                Invoke("ActivateEffect", onTime);
                 changed = true;            
             }
         }   
+    }
+
+
+    void ActivateEffect(){
+        SoundManager.instance.Activate3DSound(index, atTime);
     }
 }
--- Assets/Scripts/Triggers/TriggerSound.cs
+++ Assets/Scripts/Triggers/TriggerSound.cs
@@ -8,14 +8,22 @@ public class TriggerSound : MonoBehaviour
 
     [SerializeField]
     int index = 0;
+
+    [SerializeField]
+    [Range (0.0f, 20.0f)]
+    float secondsToPlay = 1.0f;
     // Update is called once per frame
     void Update()
     {
         if(!changed){
             if(this.transform.parent.name == "CurrentPanel"){
-                SoundManager.instance.ChangeToSound(index);
+                Invoke("PlaySound", secondsToPlay);
                 changed = true;
             }
         }
+    }
+
+    void PlaySound(){
+        SoundManager.instance.ChangeToSound(index);
     }
 }
